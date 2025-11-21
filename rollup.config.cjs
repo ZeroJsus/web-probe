b3f4b0080c8add08401f9727a3497aae9f4a7ad0
rongxin <f723706588@qq.com>
2025-11-21 15:29:53 +0800

    Copy standalone page into dist output

---
+++ rollup.config.cjs
@@ -0,0 +1,38 @@
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const { babel } = require('@rollup/plugin-babel');
+const copy = require('rollup-plugin-copy');
+
+module.exports = {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/probe.iife.js',
+    format: 'iife',
+    name: 'WebProbe',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: '>0.25%, not dead, not op_mini all',
+            bugfixes: true,
+            modules: false
+          }
+        ]
+      ],
+      extensions: ['.js'],
+      comments: false
+    }),
+    copy({
+      targets: [
+        { src: 'index.html', dest: 'dist' }
+      ]
+    })
+  ]
+};
